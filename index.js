f69b4cd574fbccae05f80a8b82cba5f957ba6335
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-20 12:19:48 +0200

    introduction js

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("hello world");
+
+// Exo 2
+
+// let sentence = "My name is Walid"
+// console.log(sentence);
+
+// Exo 3
+
+// const name = "Walid"
+// console.log(`Nice to meet you ${name}`);
+
+// exo 4
+
+// const sentence = `I'm a new text`
+// console.log(sentence.length);
+
+// exo 5
+// let food = "croissant is meh"
+// console.log(food.replace("meh", 'good'));
+
+// exo 6
+const basic = `This is cool`
+const basicUp = `BASIC`
+const basicDown = `basic`
+console.log(basic, basicUp, basicDown);
